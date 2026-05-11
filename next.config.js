18060073897d7b111ac1d88239e0d176dc347a0d
GiuseppeGDG <giuseppedelgaudio17@gmail.com>
2026-05-11 12:31:41 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-    NEXT_PUBLIC_GEMINI_API_KEY: process.env.NEXT_PUBLIC_GEMINI_API_KEY,
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
